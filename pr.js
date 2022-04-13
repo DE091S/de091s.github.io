01eb75836b30cf2d46cab4d5071ede57102dd88a
DEON1S <80581025+DE091S@users.noreply.github.com>
2022-04-13 23:01:56 +0300

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,58 @@
+const putin = document.getElementById('putin');
+const navalny = document.getElementById('navalny');
+const counter = document.querySelector('#counter')
+//const input = document.querySelector('input');
+
+document.addEventListener("click", function(event){
+    jump();
+});
+let rec = 0;
+let result = 0;
+function jump() {
+    if (putin.classList != "jump") {
+        putin.classList.add("jump")
+
+        result++;
+        if(result>rec){
+            rec = result;
+        }
+
+        counter.textContent = result;
+        //if(result>15){
+            //alert("Ты своровал все что мог");
+            //result = 0;
+        //}
+
+    }
+
+    setTimeout(function(){
+        putin.classList.remove("jump")
+    }, 250)
+}
+
+
+let isAlive = setInterval(function(){
+    let putinTop = parseInt(window.getComputedStyle(putin).getPropertyValue("top"));
+    let navalnyLeft = parseInt(window.getComputedStyle(navalny).getPropertyValue("Left"));
+    
+
+    if(navalnyLeft < 40 && navalnyLeft > 0 && putinTop >= 150){
+
+        alert("Рекорд:" +rec);
+
+        
+        //document.write("gj lvl 1 done");
+        result = 0;
+
+        
+    
+
+
+        result = 0;
+        counter.textContent = result;
+    }
+
+
+}, 10)
+
+('Button').tilt({scale: 1.1, spped: 1000});
